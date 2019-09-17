6d10edc989a0d60be5c9cf0798e7888c3f8a2761
programmerPatient <494429797@qq.com>
2019-09-17 10:23:29 +0800

    报表授权

--- app/Http/Controllers/Admin/TableController.php
+++ app/Http/Controllers/Admin/TableController.php
@@ -183,7 +183,7 @@ class TableController extends Controller
                     ];
                 }
             }
-            return view('admin.table.auth',compact('p'));//展示报表列表
+            return view('admin.table.tableauAuth',compact('p'));//展示报表列表
         }
     }
 }
