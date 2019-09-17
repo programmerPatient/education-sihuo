d23fe130e0ed7c93a9a12800e7c647a128b6dd63
programmerPatient <494429797@qq.com>
2019-09-17 09:00:04 +0800

    first push

---
+++ app/Models/Admin/Member.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Model;
+//引入trait
+use Illuminate\Auth\Authenticatable;
+
+class Member extends Model implements \Illuminate\Contracts\Auth\Authenticatable
+{
+    protected $table = 'member';
+    //使用trait，就相当于将trait代码段复制到这个位置
+    use Authenticatable;
+
+    protected $fillable =['username','password','gender','email','mobile','tableau_id','created_at','status'];
+}
