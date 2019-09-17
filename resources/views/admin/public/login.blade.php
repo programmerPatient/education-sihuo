956b1019ccf1b72aa662ed615fcc7bf55e29effd
programmerPatient <494429797@qq.com>
2019-09-17 16:33:41 +0800

    sss

--- resources/views/admin/public/login.blade.php
+++ resources/views/admin/public/login.blade.php
@@ -31,40 +31,44 @@
 <div class="loginWraper">
   <div id="loginform" class="loginBox">
     <form class="form form-horizontal" action="/admin/public/check" method="post">
-      <div class="row cl">
-        <img class="form-label col-xs-4" style="color:white;font-size:20px" src="{{$system->logo_img}}" alt="">
-        <label class="form-label col-xs-8">{{$system->web_title}}</label>
+      <div class="col-xs-3">
+        <img src="{{$system->logo_url}}" alt="logo">
       </div>
-      <div class="row cl">
-        <label class="form-label col-xs-3"><i class="Hui-iconfont">&#xe60d;</i></label>
-        <div class="formControls col-xs-8">
-          <input id="" name="username" type="text" placeholder="账户" class="input-text size-L">
+        <div class="col-xs-9">
+          <div class="row cl" style="text-align:center;">
+          <label class="form-label col-xs-8">{{$system->web_title}}</label>
         </div>
-      </div>
-      <div class="row cl">
-        <label class="form-label col-xs-3"><i class="Hui-iconfont">&#xe60e;</i></label>
-        <div class="formControls col-xs-8">
-          <input id="" name="password" type="password" placeholder="密码" class="input-text size-L">
+        <div class="row cl">
+          <label class="form-label col-xs-3"><i class="Hui-iconfont">&#xe60d;</i></label>
+          <div class="formControls col-xs-8">
+            <input id="" name="username" type="text" placeholder="账户" class="input-text size-L">
+          </div>
         </div>
-      </div>
-<!--       <div class="row cl">
-        <label class="form-label col-xs-3"><i class="Hui-iconfont">&#xe63f;</i></label>
-        <div class="formControls col-xs-8">
-          <input class="input-text size-L" type="text" name="captcha" placeholder="验证码" onblur="if(this.value==''){this.value='验证码:'}" onclick="if(this.value=='验证码:'){this.value='';}" value="验证码:" style="width:150px;">
-          <img src="{{ captcha_src() }}"> <a id="kanbuq" href="javascript:;">看不清，换一张</a> </div>
-      </div> -->
-      {{csrf_field()}}
-      <div class="row cl">
-        <div class="formControls col-xs-8 col-xs-offset-3">
-          <label for="online">
-            <input type="checkbox" name="online" id="online" value="1">
-            使我保持登录状态</label>
+        <div class="row cl">
+          <label class="form-label col-xs-3"><i class="Hui-iconfont">&#xe60e;</i></label>
+          <div class="formControls col-xs-8">
+            <input id="" name="password" type="password" placeholder="密码" class="input-text size-L">
+          </div>
         </div>
-      </div>
-      <div class="row cl">
-        <div class="formControls col-xs-8 col-xs-offset-3">
-          <input name="" type="submit" class="btn btn-success radius size-L" value="&nbsp;登&nbsp;&nbsp;&nbsp;&nbsp;录&nbsp;">
-          <input name="" type="reset" class="btn btn-default radius size-L" value="&nbsp;取&nbsp;&nbsp;&nbsp;&nbsp;消&nbsp;">
+  <!--       <div class="row cl">
+          <label class="form-label col-xs-3"><i class="Hui-iconfont">&#xe63f;</i></label>
+          <div class="formControls col-xs-8">
+            <input class="input-text size-L" type="text" name="captcha" placeholder="验证码" onblur="if(this.value==''){this.value='验证码:'}" onclick="if(this.value=='验证码:'){this.value='';}" value="验证码:" style="width:150px;">
+            <img src="{{ captcha_src() }}"> <a id="kanbuq" href="javascript:;">看不清，换一张</a> </div>
+        </div> -->
+        {{csrf_field()}}
+        <div class="row cl">
+          <div class="formControls col-xs-8 col-xs-offset-3">
+            <label for="online">
+              <input type="checkbox" name="online" id="online" value="1">
+              使我保持登录状态</label>
+          </div>
+        </div>
+        <div class="row cl">
+          <div class="formControls col-xs-8 col-xs-offset-3">
+            <input name="" type="submit" class="btn btn-success radius size-L" value="&nbsp;登&nbsp;&nbsp;&nbsp;&nbsp;录&nbsp;">
+            <input name="" type="reset" class="btn btn-default radius size-L" value="&nbsp;取&nbsp;&nbsp;&nbsp;&nbsp;消&nbsp;">
+          </div>
         </div>
       </div>
     </form>
