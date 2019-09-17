d1c9b7b91fd5271e8e13cf74ce4454253796ec0e
programmerPatient <494429797@qq.com>
2019-09-17 16:45:03 +0800

    sss

--- resources/views/admin/public/login.blade.php
+++ resources/views/admin/public/login.blade.php
@@ -32,7 +32,7 @@
   <div id="loginform" class="loginBox">
     <form class="form form-horizontal" action="/admin/public/check" method="post">
         <div class="row cl" style="text-align:center;">
-          <div class="form-label col-xs-8">{{$system->web_title}}<img class="col-xs-4" src="{{$system->logo_url}}" alt="logo" style="width:40%;height:50px;margin-left:10%;"></div>
+          <div class="form-label col-xs-8">{{$system->web_title}}<img class="col-xs-4" src="{{$system->logo_url}}" alt="logo" style="width:40%;height:50px;margin-left:30%;"></div>
         </div>
         <div class="row cl">
           <label class="form-label col-xs-3"><i class="Hui-iconfont">&#xe60d;</i></label>
