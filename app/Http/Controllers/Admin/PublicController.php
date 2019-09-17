e3c45dafab9c0f174520541764d5c7e7674b0817
programmerPatient <494429797@qq.com>
2019-09-17 13:47:43 +0800

    sss

--- app/Http/Controllers/Admin/PublicController.php
+++ app/Http/Controllers/Admin/PublicController.php
@@ -70,7 +70,7 @@ class PublicController extends Controller
             $result = Auth::guard('member') -> attempt($data,$request -> get('online'));
             $type = '2';
         }
-
+        dd(Auth::guard('membre')->check());
         Session::put('user_type',$type);
         //判断是否成功
         if($result){
