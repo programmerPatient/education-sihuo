a6f768d1d23e27b2db3bf05e7cc7fc26a025fe4b
programmerPatient <494429797@qq.com>
2019-09-17 15:41:36 +0800

    sss

--- resources/views/admin/public/login.blade.php
+++ resources/views/admin/public/login.blade.php
@@ -67,10 +67,7 @@
 <script type="text/javascript" src="/admin/static/h-ui/js/H-ui.min.js"></script>
 <script type="text/javascript" src="/admin/lib/layer/2.4/layer.js"></script>
 <script type="text/javascript">
-  history.pushState(null, null, document.URL);
-        window.addEventListener('popstate', function () {
-            history.pushState(null, null, document.URL);
-        });
+
   //jquery的载入事件
   $(function(){
     //给kanbuq绑定点击事件
