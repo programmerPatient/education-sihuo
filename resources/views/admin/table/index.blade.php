84e7c5ae5005912499f60a745786aa1a83130f60
programmerPatient <494429797@qq.com>
2019-09-20 13:58:47 +0800

    sss

--- resources/views/admin/table/index.blade.php
+++ resources/views/admin/table/index.blade.php
@@ -57,13 +57,6 @@
             <param name='filter' value='iframeSizedToWindow=true' />
         </object>
     </div>
-    <footer class="footer mt-20">
-    <div class="container">
-        <p>感谢jQuery、layer、laypage、Validform、UEditor、My97DatePicker、iconfont、Datatables、WebUploaded、icheck、highcharts、bootstrap-Switch<br>
-            Copyright &copy;2015-2017 H-ui.admin v3.1 All Rights Reserved.<br>
-            本后台系统由<a href="http://www.h-ui.net/" target="_blank" title="H-ui前端框架">H-ui前端框架</a>提供前端技术支持</p>
-    </div>
-</footer>
 <script type="text/javascript" src="/admin/lib/jquery/1.9.1/jquery.min.js"></script>
 <script type="text/javascript" src="/admin/static/h-ui/js/H-ui.min.js"></script>
 <!-- <script type="text/javascript">
