8fd3320cc87f8d3b99d2787f6ee61aaef95acaad
programmerPatient <494429797@qq.com>
2019-10-18 14:51:23 +0800

    ss

--- app/Http/Controllers/Admin/MemberController.php
+++ app/Http/Controllers/Admin/MemberController.php
@@ -13,7 +13,7 @@ class MemberController extends Controller
     //列表展示
     public function index(){
         //查询数据
-        $data = Member::all();
+        $data = Member::orderBy('id','desc')->get();
         //展示视图
         return view('admin.member.index',compact('data'));
     }
