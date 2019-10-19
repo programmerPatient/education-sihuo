2a8076bf6e885190641703d846722e04ae02d5e9
programmerPatient <494429797@qq.com>
2019-10-19 17:06:12 +0800

    ss

--- resources/views/admin/member/index.blade.php
+++ resources/views/admin/member/index.blade.php
@@ -247,28 +247,31 @@ function datadel(){
     $("input[name='ids']:checked").each(function(){
         ids.push($(this).val());
     });
-    layer.confirm('确认要删除吗？',function(index){
-        $.ajax({
-            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-            type: 'delete',
-            url: '/admin/members/delete',
-            data:{'ids':ids},
-            dataType: 'json',
-            success: function(data){
-                if(data == '1')
-                {
-
-                    layer.msg('批量删除成功!',{icon:1,time:1000});
-                    window.location = window.location;
-                }else{
-                    layer.msg('批量删除失败，请注意查看!',{icon:1,time:1000});
-                }
-            },
-            error:function(data) {
-                console.log(data.msg);
-            },
+    if(!ids){
+        layer.msg('请选择要批量删除的对象!',{icon:1,time:1000});
+    }else{
+        layer.confirm('确认要删除吗？',function(index){
+            $.ajax({
+                headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
+                type: 'delete',
+                url: '/admin/members/delete',
+                data:{'ids':ids},
+                dataType: 'json',
+                success: function(data){
+                    if(data == '1')
+                    {
+                        layer.msg('批量删除成功!',{icon:1,time:1000});
+                        window.location = window.location;
+                    }else{
+                        layer.msg('批量删除失败，请注意查看!',{icon:1,time:1000});
+                    }
+                },
+                error:function(data) {
+                    console.log(data.msg);
+                },
+            });
         });
-    });
+    }
 }
 </script>
 </body>
