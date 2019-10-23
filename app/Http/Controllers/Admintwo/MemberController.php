faeed45b1a4efb562d6004b28054484536221ea5
programmerPatient <494429797@qq.com>
2019-10-23 18:33:50 +0800

    s

--- app/Http/Controllers/Admintwo/MemberController.php
+++ app/Http/Controllers/Admintwo/MemberController.php
@@ -27,41 +27,43 @@ class MemberController extends Controller
             $data['password'] = bcrypt($data['password']);
             $data['status'] = Input::get(['tableau_user']);
             $error = array();
+            $status = '1';
             $result = Member::where('username',$data['username'])->get()->first();
             if($result){
                 $error[] = $data['username'];
                 $status = '0';
-            }
-            if(Input::get(['tableau_user']) == '1'){
-                //创建table用户
-                $curl = curl_init();
+            }else{
+                if(Input::get(['tableau_user']) == '1'){
+                    //创建table用户
+                    $curl = curl_init();
 
-                curl_setopt_array($curl, array(
-                CURLOPT_URL => "http://tableau.kalaw.top/api/3.2/sites/".Session::get('credentials')."/users",
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => "",
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 30,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "POST",
-                CURLOPT_POSTFIELDS => "<tsRequest><user name=\"" . $data['username'] ."\" siteRole=\"Interactor\" authSetting=\"ServerDefault\" /></tsRequest>",
-                CURLOPT_HTTPHEADER => array(
-                    "X-Tableau-Auth: ". Session::get('token'),
-                    "Accept: application/json",
-                  ),
-                ));
-                $response = curl_exec($curl);
-                $err = curl_error($curl);
-                curl_close($curl);
-                if ($err) {
-                  echo "cURL Error #:" . $err;
-                } else {
-                    $da = json_decode($response);
-                    $data['tableau_id'] = $da->user->id;
+                    curl_setopt_array($curl, array(
+                    CURLOPT_URL => "http://tableau.kalaw.top/api/3.2/sites/".Session::get('credentials')."/users",
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => "",
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 30,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => "POST",
+                    CURLOPT_POSTFIELDS => "<tsRequest><user name=\"" . $data['username'] ."\" siteRole=\"Interactor\" authSetting=\"ServerDefault\" /></tsRequest>",
+                    CURLOPT_HTTPHEADER => array(
+                        "X-Tableau-Auth: ". Session::get('token'),
+                        "Accept: application/json",
+                      ),
+                    ));
+                    $response = curl_exec($curl);
+                    $err = curl_error($curl);
+                    curl_close($curl);
+                    if ($err) {
+                      echo "cURL Error #:" . $err;
+                    } else {
+                        $da = json_decode($response);
+                        $data['tableau_id'] = $da->user->id;
+                    }
                 }
+                // $data['avatar'] = "/images/th.jpg";
+                $result = Member::insert($data);
             }
-            // $data['avatar'] = "/images/th.jpg";
-            $result = Member::insert($data);
             $da['error'] = $error;
             $da['status'] = $status;
             return $da;
