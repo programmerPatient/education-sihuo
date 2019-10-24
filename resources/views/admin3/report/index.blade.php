e3c2e0b828db73c916bfda6913d8e6ff95b8a496
programmerPatient <494429797@qq.com>
2019-10-24 11:48:39 +0800

    s

--- resources/views/admin3/report/index.blade.php
+++ resources/views/admin3/report/index.blade.php
@@ -34,11 +34,11 @@
         <div class="row cl">
             <label class="form-label col-xs-4 col-sm-3"><span class="c-red">*</span>用户名：</label>
             @if(isset($project_group))
-                @foreach($project_group as $val)
-                    <div id="inputarray" class="formControls col-xs-8 col-sm-9">
+                <div id="inputarray" class="formControls col-xs-8 col-sm-9">
+                    @foreach($project_group as $val)
                         <textarea class="input-text" style="margin-top:20px;height:40px;width:500px;" value="" placeholder="" id="project_group" name="project_group[]">{{$val}}</textarea>
-                    </div>
-                @endforeach
+                    @endforeach
+                </div>
             @else
                <div id="inputarray" class="formControls col-xs-8 col-sm-9">
                     <textarea class="input-text" style="margin-top:20px;height:40px;width:500px;" value="" placeholder="" id="project_group" name="project_group[]"></textarea>
