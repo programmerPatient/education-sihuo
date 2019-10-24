16be0742eda3284ab4314bae5f1ce8fd65b37065
programmerPatient <494429797@qq.com>
2019-10-24 11:19:22 +0800

    s

--- resources/views/admin3/report/index.blade.php
+++ resources/views/admin3/report/index.blade.php
@@ -110,15 +110,15 @@ $(function(){
                 type: 'post',
                 url: "" ,//自己提交给自己可以不写url
                 success: function(data){
-                    if(data['status'] == '1'){
-                        layer.msg('添加成功!',{icon:1,time:1000},function(){
+                    if(data == '1'){
+                        layer.msg('项目组映射成功!',{icon:1,time:1000},function(){
                             var index = parent.layer.getFrameIndex(window.name);
                             //刷新
                             parent.window.location = parent.window.location;
                             parent.layer.close(index);
                         });
                     }else{
-                        layer.msg('添加失败,以下名字重复： '+data['error'][0],{icon:2,time:2000});
+                        layer.msg('项目组映射失败',{icon:2,time:2000});
                     }
                 },
                 error: function(XmlHttpRequest, textis_nav, errorThrown){
