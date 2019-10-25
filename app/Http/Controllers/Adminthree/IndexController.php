0100abf2f275f33f004b583fb5f7ca449ec2d6d9
programmerPatient <494429797@qq.com>
2019-10-25 23:04:30 +0800

    ss

--- app/Http/Controllers/Adminthree/IndexController.php
+++ app/Http/Controllers/Adminthree/IndexController.php
@@ -99,7 +99,9 @@ class IndexController extends Controller
                         foreach($dat as $g=>$r){
                             if($viesdata[$i]->id == $r->report_id){
                                 if($r->project_group){
-                                    dd(RelationMember::where('member_id',$user->id)->get()->first()->project_group.'|'.$r->project_group);
+                                    if($user){
+                                        $r->project_group=RelationMember::where('member_id',$user->id)->get()->first()->project_group.'|'.$r->project_group
+                                    }
                                     $viesdata[$i]->filter = implode('&',explode('|',$r->project_group));
                                 }else{
                                     $viesdata[$i]->filter = "iframeSizedToWindow=true";
