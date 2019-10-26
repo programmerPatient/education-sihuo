0264b944eb44e728c2de907c35cba05833b30c96
programmerPatient <494429797@qq.com>
2019-10-26 12:18:43 +0800

    s

--- app/Http/Controllers/Adminthree/IndexController.php
+++ app/Http/Controllers/Adminthree/IndexController.php
@@ -108,7 +108,7 @@ class IndexController extends Controller
                                                     continue;
                                                 }else{
                                                     if(explode('=',$proj)[0] == explode('=',$proj2)[0]){
-                                                        $pro[$po1] = explode('=',$proj)[1] .',' . explode('=',$proj2)[1];
+                                                        $pro[$po] = explode('=',$proj)[1] .',' . explode('=',$proj2)[1];
                                                     }
                                                 }
                                             }
