4a40a1e4591d8b9cbc9ccd9891b8982c3dd1fa71
programmerPatient <494429797@qq.com>
2019-10-27 21:57:16 +0800

    s

--- app/Http/Controllers/Admin/ManagerController.php
+++ app/Http/Controllers/Admin/ManagerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Admin\Manager;
+use Input;
 
 class ManagerController extends Controller
 {
