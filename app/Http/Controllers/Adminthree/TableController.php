f13660049bd4763d3b862b8c42bead46620b5260
programmerPatient <494429797@qq.com>
2019-10-28 14:49:36 +0800

    s

--- app/Http/Controllers/Adminthree/TableController.php
+++ app/Http/Controllers/Adminthree/TableController.php
@@ -185,8 +185,7 @@ class TableController extends Controller
             }
             foreach($group as $gk=>$gv){
                 if(!in_array($gv->report_id,$hasTableauIds)){
-                    $user = RelationReport::find($gv->id);
-                    $user->delete();
+                     RelationReport::find($gv->id)->delete();
                 }
             }
             RelationReport::insert($insert);
