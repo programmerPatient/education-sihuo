9b88628005d005249dbfc1956b62cfe4fda501b4
programmerPatient <494429797@qq.com>
2019-10-28 15:31:43 +0800

    s

--- resources/views/admin3/report/select.blade.php
+++ resources/views/admin3/report/select.blade.php
@@ -25,6 +25,10 @@
 <body>
 <nav class="breadcrumb"><i class="Hui-iconfont">&#xe67f;</i> 首页 <span class="c-gray en">&gt;</span> 报表定位 <a class="btn btn-success radius r" style="line-height:1.6em;margin-top:3px" href="javascript:location.replace(location.href);" title="刷新" ><i class="Hui-iconfont">&#xe68f;</i></a></nav>
 <div class="page-container">
+    <div class="cl pd-5 bg-1 bk-gray mt-20">
+        <span class="l">
+        </span>
+    </div>
     <table class="table table-border table-bordered table-hover table-bg table-sort">
         <thead>
             <tr class="text-c">
