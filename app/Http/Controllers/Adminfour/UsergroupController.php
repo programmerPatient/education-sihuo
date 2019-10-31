23866120444e45e24df56100b862e82e34b3f534
programmerPatient <494429797@qq.com>
2019-10-31 20:49:33 +0800

    s

--- app/Http/Controllers/Adminfour/UsergroupController.php
+++ app/Http/Controllers/Adminfour/UsergroupController.php
@@ -120,7 +120,7 @@ class UsergroupController extends Controller
     //删除用户组
     public function delete(){
         $id = Input::get('id');
-        ddd($id);
+        dd($id);
         $data = RelationReport::where('usergroup_id',$id)->get();
         foreach($data as $key=>$value){
             $value->usergroup_id = null;
