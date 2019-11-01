bbb049e5a5cdac2ef28aad63068952434f7eddeb
programmerPatient <494429797@qq.com>
2019-11-01 19:48:36 +0800

    s

--- resources/views/admin3/inization/system.blade.php
+++ resources/views/admin3/inization/system.blade.php
@@ -193,13 +193,13 @@ $(function(){
                 url: "/admin/public/inization" ,//自己提交给自己可以不写url
                 success: function(data){
                     if(data == '1'){
-                        layer.msg('更新成功!',{icon:1,time:1000},function(){
+                        layer.msg('提交成功!',{icon:1,time:1000},function(){
                             var index = parent.layer.getFrameIndex(window.name);
                             // //刷新
-                            window.location = window.location;
+                            window.location.reload()
                         });
                     }else{
-                        layer.msg('更新失败!',{icon:2,time:2000});
+                        layer.msg('提交失败!',{icon:2,time:2000});
                     }
                 },
                 error: function(XmlHttpRequest, textis_nav, errorThrown){
