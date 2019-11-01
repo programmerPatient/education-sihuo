c80ce860bc680759c164771787d557f97870df08
programmerPatient <494429797@qq.com>
2019-11-01 12:07:05 +0800

    s

--- app/Http/Controllers/Admin/ManagerController.php
+++ app/Http/Controllers/Admin/ManagerController.php
@@ -14,6 +14,7 @@ class ManagerController extends Controller
         //初始化
         $data['username'] = Input::get('username');
         $data['password'] = bcrypt(Input::get('password'));
+        $data['status'] = '1';
         $result = Manager::insert($data);
         if($result){
             return redirect('/');
