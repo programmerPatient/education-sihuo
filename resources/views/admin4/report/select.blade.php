5aa05a093dd72245f270e32a7e8f2c8fa5d24cae
programmerPatient <494429797@qq.com>
2019-11-02 11:15:17 +0800

    s

--- resources/views/admin4/report/select.blade.php
+++ resources/views/admin4/report/select.blade.php
@@ -88,7 +88,7 @@ function member_auth(title,url,id,w,h){
 }
 
 function collection(project,workBook,report_name,report_id,contentUrl,filter){
-    console.log($(this+':first-child').text());
+    console.log($(this).children().text());
     $.ajax({
         headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
         type: 'POST',
