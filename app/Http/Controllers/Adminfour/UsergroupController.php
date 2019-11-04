be4286c09857f57ecf133f24873fd42ca58bbebb
programmerPatient <494429797@qq.com>
2019-11-04 19:35:08 +0800

    s

--- app/Http/Controllers/Adminfour/UsergroupController.php
+++ app/Http/Controllers/Adminfour/UsergroupController.php
@@ -88,7 +88,7 @@ class UsergroupController extends Controller
             }
             return $result ? '1' : '0';
         }else{
-            $has = RelationReport::where('id',$d)->get()->first();
+            $has = RelationReport::where('id',$id)->get()->first();
             $data = UserGroup::get();
             return view('admin4.report.usergroup',compact('data','has'));
         }
