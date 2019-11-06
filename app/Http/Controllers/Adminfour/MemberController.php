60c307ceb6e0380a2c6af2e93a7fdc0db7350d87
programmerPatient <494429797@qq.com>
2019-11-06 11:39:50 +0800

    s

--- app/Http/Controllers/Adminfour/MemberController.php
+++ app/Http/Controllers/Adminfour/MemberController.php
@@ -191,6 +191,7 @@ class MemberController extends Controller
             return $result ? '1' : '0';
         }else{
             $user = Member::where('id',$id)->get()->first();
+            dd($user);
             return view('admin4.member.excel',compact('user'));
         }
     }
