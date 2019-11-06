0615599fed1f275679265f67d8d8b65bbe64be1f
programmerPatient <494429797@qq.com>
2019-11-06 21:32:06 +0800

    s

--- app/Http/Controllers/Admin/PublicController.php
+++ app/Http/Controllers/Admin/PublicController.php
@@ -121,6 +121,7 @@ class PublicController extends Controller
         $system = System::get()->first();
         $username = $system->tableau_username;
         $password = $system->tableau_password;
+        dd($password);
         //判断是否成功
         if($result){
             $curl = curl_init();
