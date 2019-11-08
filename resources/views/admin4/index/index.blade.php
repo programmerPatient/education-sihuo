d3c6312f8dcf174569590edb3e49ecbb4e338ab1
programmerPatient <494429797@qq.com>
2019-11-08 21:51:58 +0800

    s

--- resources/views/admin4/index/index.blade.php
+++ resources/views/admin4/index/index.blade.php
@@ -144,8 +144,9 @@
         <div class="Hui-tabNav-more btn-group"><a id="js-tabNav-prev" class="btn radius btn-default size-S" href="javascript:;"><i class="Hui-iconfont">&#xe6d4;</i></a><a id="js-tabNav-next" class="btn radius btn-default size-S" href="javascript:;"><i class="Hui-iconfont">&#xe6d7;</i></a></div>
     </div>
     <div id="iframe_box" class="Hui-article">
-        <div style="background-color: red;width:100%;height:100px;">ssssssssssssssssssssssssssss</div>
+
         <div class="show_iframe">
+            <div style="background-color: red;width:100%;height:100px;">ssssssssssssssssssssssssssss</div>
             <div style="display:none" class="loading"></div>
 
             <!-- 修改src引入地址 -->
