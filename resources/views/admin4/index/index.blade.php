b10d90155086c709f27184afbac264bfb39ac555
programmerPatient <494429797@qq.com>
2019-11-08 22:02:07 +0800

    s

--- resources/views/admin4/index/index.blade.php
+++ resources/views/admin4/index/index.blade.php
@@ -141,7 +141,7 @@
                 </li>
             </ul>
         </div>
-        <div class="Hui-tabNav-more btn-group"><a id="js-tabNav-prev" class="btn radius btn-default size-S" href="javascript:;" onClick="dd()"><i class="Hui-iconfont">&#xe6d4;</i></a><a id="js-tabNav-next" class="btn radius btn-default size-S" href="javascript:;"><i class="Hui-iconfont">&#xe6d7;</i></a></div>
+        <div class="Hui-tabNav-more btn-group"><a id="js-tabNav-prev" class="btn radius btn-default size-S" href="javascript:;"><i class="Hui-iconfont">&#xe6d4;</i></a><a id="js-tabNav-next" class="btn radius btn-default size-S" href="javascript:;"><i class="Hui-iconfont">&#xe6d7;</i></a></div>
     </div>
     <div id="iframe_box" class="Hui-article">
 
