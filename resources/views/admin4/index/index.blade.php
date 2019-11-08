b7a7798d65e04c6a2275e5da09015381182d75d3
programmerPatient <494429797@qq.com>
2019-11-08 20:12:49 +0800

    s

--- resources/views/admin4/index/index.blade.php
+++ resources/views/admin4/index/index.blade.php
@@ -130,25 +130,26 @@
 </aside>
 <div class="dislpayArrow hidden-xs"><a class="pngfix" href="javascript:void(0);" onClick="displaynavbar(this)"></a></div>
 <section class="Hui-article-box">
-    <div id="Hui-tabNav" class="Hui-tabNav hidden-xs">
-        <div class="Hui-tabNav-wp">
-            <ul id="min_title_list" class="acrossTab cl">
-                <li class="active">
+    <!-- <div id="Hui-tabNav" class="Hui-tabNav hidden-xs"> -->
+        <!-- <div class="Hui-tabNav-wp"> -->
+            <!-- <ul id="min_title_list" class="acrossTab cl"> -->
+                <!-- <li class="active"> -->
                     <!-- 修改src引入地址 -->
                     <!-- <span title="我的收藏" data-href="/adminfour/index//admin/index/welcome">我的桌面</span> -->
-                    <em></em></li>
-        </ul>
-    </div>
-        <div class="Hui-tabNav-more btn-group"><a id="js-tabNav-prev" class="btn radius btn-default size-S" href="javascript:;"><i class="Hui-iconfont">&#xe6d4;</i></a><a id="js-tabNav-next" class="btn radius btn-default size-S" href="javascript:;"><i class="Hui-iconfont">&#xe6d7;</i></a></div>
-</div>
+                    <!-- <em></em> -->
+                <!-- </li> -->
+            <!-- </ul> -->
+        <!-- </div> -->
+        <!-- <div class="Hui-tabNav-more btn-group"><a id="js-tabNav-prev" class="btn radius btn-default size-S" href="javascript:;"><i class="Hui-iconfont">&#xe6d4;</i></a><a id="js-tabNav-next" class="btn radius btn-default size-S" href="javascript:;"><i class="Hui-iconfont">&#xe6d7;</i></a></div> -->
+    <!-- </div> -->
     <div id="iframe_box" class="Hui-article">
         <div class="show_iframe">
             <div style="display:none" class="loading"></div>
 
             <!-- 修改src引入地址 -->
             <iframe scrolling="yes" frameborder="0" src="/adminfour/report/collection/index"></iframe>
+        </div>
     </div>
-</div>
 </section>
 
 <div class="contextMenu" id="Huiadminmenu">
