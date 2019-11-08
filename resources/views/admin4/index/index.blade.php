caee59bffa2d0797cc4c3411a3b9c28f1d474ac5
programmerPatient <494429797@qq.com>
2019-11-08 20:16:38 +0800

    s

--- resources/views/admin4/index/index.blade.php
+++ resources/views/admin4/index/index.blade.php
@@ -131,16 +131,16 @@
 <div class="dislpayArrow hidden-xs"><a class="pngfix" href="javascript:void(0);" onClick="displaynavbar(this)"></a></div>
 <section class="Hui-article-box">
     <div id="Hui-tabNav" class="Hui-tabNav hidden-xs">
-        <!-- <div class="Hui-tabNav-wp"> -->
-            <!-- <ul id="min_title_list" class="acrossTab cl"> -->
-                <!-- <li class="active"> -->
+        <div class="Hui-tabNav-wp">
+            <ul id="min_title_list" class="acrossTab cl">
+                <li class="active">
                     <!-- 修改src引入地址 -->
-                    <!-- <span title="我的收藏" data-href="/adminfour/index//admin/index/welcome">我的桌面</span> -->
-                    <!-- <em></em> -->
-                <!-- </li> -->
-            <!-- </ul> -->
-        <!-- </div> -->
-        <!-- <div class="Hui-tabNav-more btn-group"><a id="js-tabNav-prev" class="btn radius btn-default size-S" href="javascript:;"><i class="Hui-iconfont">&#xe6d4;</i></a><a id="js-tabNav-next" class="btn radius btn-default size-S" href="javascript:;"><i class="Hui-iconfont">&#xe6d7;</i></a></div> -->
+                    <span title="我的收藏" data-href="/adminfour/index//admin/index/welcome">我的桌面</span>
+                    <em></em>
+                </li>
+            </ul>
+        </div>
+        <div class="Hui-tabNav-more btn-group"><a id="js-tabNav-prev" class="btn radius btn-default size-S" href="javascript:;"><i class="Hui-iconfont">&#xe6d4;</i></a><a id="js-tabNav-next" class="btn radius btn-default size-S" href="javascript:;"><i class="Hui-iconfont">&#xe6d7;</i></a></div>
     </div>
     <div id="iframe_box" class="Hui-article">
         <div class="show_iframe">
