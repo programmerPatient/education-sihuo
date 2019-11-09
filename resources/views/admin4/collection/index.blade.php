e4fe6a22866e76ab63c9c73e82ad93087be8f388
programmerPatient <494429797@qq.com>
2019-11-09 18:59:45 +0800

    s

--- resources/views/admin4/collection/index.blade.php
+++ resources/views/admin4/collection/index.blade.php
@@ -23,7 +23,7 @@
 <title>收藏列表</title>
 </head>
 <body>
-<nav class="breadcrumb"><i class="Hui-iconfont">&#xe67f;</i> 首页  <span class="c-gray en">&gt;</span> 收藏列表 <a class="btn btn-danger radius r" style="line-height:1.6em;margin-top:3px;margin-left: 5px" href="javascript:;" title="退出账户" onClick="suaxin()" ><i class="Hui-iconfont">&#xe726;</i></a><a class="btn btn-success radius r" style="line-height:1.6em;margin-top:3px" href="javascript:location.replace(location.href);" title="刷新" ><i class="Hui-iconfont">&#xe68f;</i></a></nav>
+<nav class="breadcrumb"><i class="Hui-iconfont">&#xe67f;</i> 首页  <span class="c-gray en">&gt;</span> 收藏列表<input type="text" placeholder="请输入搜索报表名称" onchange="InstantSearch(this)"> <a class="btn btn-danger radius r" style="line-height:1.6em;margin-top:3px;margin-left: 5px" href="javascript:;" title="退出账户" onClick="suaxin()" ><i class="Hui-iconfont">&#xe726;</i></a><a class="btn btn-success radius r" style="line-height:1.6em;margin-top:3px" href="javascript:location.replace(location.href);" title="刷新" ><i class="Hui-iconfont">&#xe68f;</i></a></nav>
 <div class="page-container">
    <!--  <div class="cl pd-5 bg-1 bk-gray mt-20">
         <span class="l">
@@ -84,6 +84,10 @@
 <script type="text/javascript" src="/admin/lib/datatables/1.10.0/jquery.dataTables.min.js"></script>
 <script type="text/javascript" src="/admin/lib/laypage/1.2/laypage.js"></script>
 <script type="text/javascript">
+function InstantSearch(){
+
+}
+
 $(function(){
     $('.table-sort').dataTable({
         "aaSorting": [[ 1, "desc" ]],//默认第几个排序
@@ -93,6 +97,10 @@ $(function(){
           {"orderable":false,"aTargets":[0,7]}// 制定列不参与排序
         ]
     });
+
+    function InstantSearch(obj){
+        console.log(obj.value());
+    }
 
 });
 
