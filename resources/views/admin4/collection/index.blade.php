b3b7004e282b9cb859b9b32879192a49dc89e2c8
programmerPatient <494429797@qq.com>
2019-11-09 19:52:46 +0800

    s

--- resources/views/admin4/collection/index.blade.php
+++ resources/views/admin4/collection/index.blade.php
@@ -94,12 +94,12 @@ function InstantSearch(obj){
             dataType: 'json',
             success: function(data){
                 console.log(data);
-                $('table').remove();
-                var num = '';
-                for var i=0;i<data.length;i++{
-                    num += '<div class="col-xs-3 col-sm-3">'+data[i].project_name+'</div>'
-                }
-                $('.page-container').append('<div class="col-xs-12 col-sm-12">'+num+'</div>');
+                // $('table').remove();
+                // var num = '';
+                // for var i=0;i<data.length;i++{
+                //     num += '<div class="col-xs-3 col-sm-3">'+data[i].project_name+'</div>'
+                // }
+                // $('.page-container').append('<div class="col-xs-12 col-sm-12">'+num+'</div>');
 
             },
             error:function(data) {
