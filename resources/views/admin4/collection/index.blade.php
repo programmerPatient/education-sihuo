6108cf7e28a0477b440917f88a91faa0a1124f88
programmerPatient <494429797@qq.com>
2019-11-09 19:53:25 +0800

    s

--- resources/views/admin4/collection/index.blade.php
+++ resources/views/admin4/collection/index.blade.php
@@ -94,11 +94,11 @@ function InstantSearch(obj){
             dataType: 'json',
             success: function(data){
                 console.log(data);
-                // $('table').remove();
-                // var num = '';
-                // for var i=0;i<data.length;i++{
-                //     num += '<div class="col-xs-3 col-sm-3">'+data[i].project_name+'</div>'
-                // }
+                $('table').remove();
+                var num = '';
+                for var i=0;i<data.length;i++{
+                    num += '<div class="col-xs-3 col-sm-3">'+data[i].project_name+'</div>'
+                }
                 // $('.page-container').append('<div class="col-xs-12 col-sm-12">'+num+'</div>');
 
             },
