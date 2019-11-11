4fb5866c49f5456157dac3e834c20c2994bba855
programmerPatient <494429797@qq.com>
2019-11-11 19:16:44 +0800

    s

--- app/Http/Controllers/Admin/PublicController.php
+++ app/Http/Controllers/Admin/PublicController.php
@@ -121,7 +121,7 @@ class PublicController extends Controller
         $system = System::get()->first();
         $username = $system->tableau_username;
         $password = $system->tableau_password;
-        dd(Session::get('tableau_domain'));
+        dd($username);
         // dd($password);
         //判断是否成功
         if($result){
