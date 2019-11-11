985de344482a3b389488dfbdb422176ea96d9ce4
programmerPatient <494429797@qq.com>
2019-11-11 21:13:36 +0800

    s

--- app/Http/Controllers/Adminfour/TableController.php
+++ app/Http/Controllers/Adminfour/TableController.php
@@ -27,7 +27,7 @@ class TableController extends Controller
         }else{
             $username = $name->tableau_id;
         }
-        // echo "".$username."and".Session::get('tableau_domain');
+        echo "".$username."and".Session::get('tableau_domain');
         // dd(Session::get('tableau_domain'));
 
         $curl = curl_init();
