d7e39007d72ec1cc283eaed8d29a8c9c78709ada
programmerPatient <494429797@qq.com>
2019-11-11 20:43:04 +0800

    s

--- app/Http/Controllers/Adminfour/TableController.php
+++ app/Http/Controllers/Adminfour/TableController.php
@@ -68,7 +68,7 @@ class TableController extends Controller
         array_splice($array,1,1);
         $contentUrl = implode("/", $array);
         // $ticket = Session::get('ticket');
-        dd($ticket);
+        // dd($ticket);
         $report_id = $request->id;
         $toolbar = System::get()->first()->toolbar;
         return view('admin4.table.index',compact('contentUrl','ticket','filter','toolbar','report_id','hascontentUrl'));
