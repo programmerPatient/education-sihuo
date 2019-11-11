dfe2b9567e6d0bb765d72703f8be6a5d184148cd
programmerPatient <494429797@qq.com>
2019-11-11 12:28:55 +0800

    s

--- app/Http/Controllers/Adminfour/ReportController.php
+++ app/Http/Controllers/Adminfour/ReportController.php
@@ -432,7 +432,7 @@ class ReportController extends Controller
         }
 
         if(!$ls){
-            $result = RelationReport::where('member_id',$user->id)->where(function($query){
+            $result = RelationReport::where('member_id',$user->id)->where(function($query,$conditions){
                 $query->where('report_name','like','%'.$conditions.'%')->orwhere('project_name','like','%'.$conditions.'%')->orwhere('workBook_name','like','%'.$conditions.'%');
             })->get();
             // dd($result);
