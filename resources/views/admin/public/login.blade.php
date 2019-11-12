2a10f28055a4a4b90b2f1b7f6731f7e34c67e649
programmerPatient <494429797@qq.com>
2019-11-12 10:15:58 +0800

    s

--- resources/views/admin/public/login.blade.php
+++ resources/views/admin/public/login.blade.php
@@ -73,7 +73,7 @@
         </div>
       </div>
     </form>
-      <div class="footer" style="background:#d8dee1;color:#000;font-size:15px;">{{$system->company}}</div>
+      <div  style="background:#d8dee1;color:#000;font-size:15px;">{{$system->company}}</div>
 
   </div>
 </div>
