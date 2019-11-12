a2584a0f47383fe70fdbe955dc38a6e603511d2a
programmerPatient <494429797@qq.com>
2019-11-12 10:50:38 +0800

    s

--- resources/views/admin4/system/index.blade.php
+++ resources/views/admin4/system/index.blade.php
@@ -387,19 +387,19 @@ $(function(){
 
         $('#cropedBigImg').attr('src',src);
     });
-     $('.file-frs').on('change',function(){
-        var filePath = $(this).val(),         //获取到input的value，里面是文件的路径
-            fileFormat = filePath.substring(filePath.lastIndexOf(".")).toLowerCase(),
-            src = window.URL.createObjectURL(this.files[0]); //转成可以在本地预览的格式
+    //  $('.file-frs').on('change',function(){
+    //     var filePath = $(this).val(),         //获取到input的value，里面是文件的路径
+    //         fileFormat = filePath.substring(filePath.lastIndexOf(".")).toLowerCase(),
+    //         src = window.URL.createObjectURL(this.files[0]); //转成可以在本地预览的格式
 
-        // 检查是否是图片
-        if( !fileFormat.match(/.png|.jpg|.jpeg/) ) {
-            error_prompt_alert('上传错误,文件格式必须为：png/jpg/jpeg');
-            return;
-        }
+    //     // 检查是否是图片
+    //     if( !fileFormat.match(/.png|.jpg|.jpeg/) ) {
+    //         error_prompt_alert('上传错误,文件格式必须为：png/jpg/jpeg');
+    //         return;
+    //     }
 
-        $('.cropedBigImgs').attr('src',src);
-    });
+    //     $('.cropedBigImgs').attr('src',src);
+    // });
 });
 
 </script>
