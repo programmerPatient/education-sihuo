ed75e568f8cf6856b49c17703b1b307e58071f0f
programmerPatient <494429797@qq.com>
2019-11-13 11:36:27 +0800

    s

--- resources/views/admin/public/login.blade.php
+++ resources/views/admin/public/login.blade.php
@@ -32,15 +32,17 @@
 <div class="loginWraper" style="background:url({{$system->background_url}});background-size: 100% 100%;
                 background-position: center center;
                 overflow: auto;">
-  <div class="formControls  toptitle" style="text-align:center;top:40px">
-      <h4 style="padding-top:10px;margin-bottom:5px;color:#fff;margin-left:50px;width:30%">{{$system->web_title}}</h4>
-    </div>
 
-  <div id="loginform" class="loginBox" style="padding-top:0;border-bottom:2px solid #6b8db6;width:500px;height:300px;border-right:2px solid #6b8db6;top:55%;left:70%">
-    <form class="form form-horizontal" action="/admin/public/check" method="post">
-        <div class="row cl" style="margin-left:70px;line-height:41px;margin-top:10px;">
+  <div class="row cl" style="margin-left:70px;line-height:41px;margin-top:10px;">
           <label class="form-label col-xs-8"><img src="{{$system->logo_url}}" alt="" style="height:80px;width:360px;"></label>
         </div>
+
+
+  <div id="loginform" class="loginBox" style="padding-top:0;border-bottom:2px solid #6b8db6;width:500px;height:300px;border-right:2px solid #6b8db6;top:55%;left:70%">
+    <div class="formControls  toptitle" style="text-align:center;top:40px">
+      <h4 style="padding-top:10px;margin-bottom:5px;color:#fff;margin-left:50px;width:30%">{{$system->web_title}}</h4>
+    </div>
+    <form class="form form-horizontal" action="/admin/public/check" method="post">
         <div class="row cl" style="margin-left:-80px;">
           <label class="form-label col-xs-3"><i class="Hui-iconfont" style="color:#f7f7f7">&#xe60d;</i></label>
           <div class="formControls col-xs-8">
