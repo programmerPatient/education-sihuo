e45aa6bf63ef30864cd851e6d80f82d3e4af479e
programmerPatient <494429797@qq.com>
2019-11-14 10:13:41 +0800

    s

--- app/Http/Controllers/Adminfour/ReportController.php
+++ app/Http/Controllers/Adminfour/ReportController.php
@@ -463,43 +463,43 @@ class ReportController extends Controller
         // dd($result);
 
 
-        //获取缩略图
-        $curlt = curl_init();
+        // //获取缩略图
+        // $curlt = curl_init();
 
-        /*获取用户的信息*/
-        curl_setopt_array($curlt, array(
-        CURLOPT_URL =>  Session::get('tableau_domain')."/api/3.2/sites/".Session::get('credentials')."/workbooks/e51bfd80-8148-49fb-8a23-b177a73beb60/previewImage",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "GET",
-        // CURLOPT_COOKIE =>"token=".Session::get('token'),
-        CURLOPT_HTTPHEADER => array(
-            "X-Tableau-Auth: ".Session::get('token'),
-            "Accept: application/json",
-          ),
-        ));
-        $response = curl_exec($curlt);
-        if(!$response) {
-                return view('admin4.error.index');
-        }
-        $err = curl_error($curlt);
-        curl_close($curlt);
-        if ($err) {
-          echo "cURL Error #:" . $err;
-        } else {
-            // dd($response);
-          // $response = simplexml_load_string($response);
+        // /*获取用户的信息*/
+        // curl_setopt_array($curlt, array(
+        // CURLOPT_URL =>  Session::get('tableau_domain')."/api/3.2/sites/".Session::get('credentials')."/workbooks/e51bfd80-8148-49fb-8a23-b177a73beb60/previewImage",
+        // CURLOPT_RETURNTRANSFER => true,
+        // CURLOPT_ENCODING => "",
+        // CURLOPT_MAXREDIRS => 10,
+        // CURLOPT_TIMEOUT => 30,
+        // CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        // CURLOPT_CUSTOMREQUEST => "GET",
+        // // CURLOPT_COOKIE =>"token=".Session::get('token'),
+        // CURLOPT_HTTPHEADER => array(
+        //     "X-Tableau-Auth: ".Session::get('token'),
+        //     "Accept: application/json",
+        //   ),
+        // ));
+        // $response = curl_exec($curlt);
+        // if(!$response) {
+        //         return view('admin4.error.index');
+        // }
+        // $err = curl_error($curlt);
+        // curl_close($curlt);
+        // if ($err) {
+        //   echo "cURL Error #:" . $err;
+        // } else {
+        //     // dd($response);
+        //   // $response = simplexml_load_string($response);
 
-        }
-        // dd($response);
-        $encode = mb_detect_encoding($response, array("ASCII",'UTF-8',"GB2312","GBK",'BIG5'));
-        $str_encode = mb_convert_encoding($response, 'UTF-8', $encode);
-        // dd($str_encode);
-        $image = $str_encode;
+        // }
+        // // dd($response);
+        // $encode = mb_detect_encoding($response, array("ASCII",'UTF-8',"GB2312","GBK",'BIG5'));
+        // $str_encode = mb_convert_encoding($response, 'UTF-8', $encode);
+        // // dd($str_encode);
+        // $image = $str_encode;
         // dd($result);
-        return $image;
+        return $result;
    }
 }
