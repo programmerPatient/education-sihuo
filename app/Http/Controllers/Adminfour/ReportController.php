fcbd0ab45eaa0311543ad5f1eca1e10a2e32f0f6
programmerPatient <494429797@qq.com>
2019-11-14 11:11:06 +0800

    s

--- app/Http/Controllers/Adminfour/ReportController.php
+++ app/Http/Controllers/Adminfour/ReportController.php
@@ -501,7 +501,7 @@ class ReportController extends Controller
         // $ss = mb_detect_encoding($str_encode, array("ASCII",'UTF-8',"GB2312","GBK",'BIG5'));
         // dd($ss);
         // dd($str_encode);
-        $ncode = json_decode($str_encode);
+        $ncode = json_encode($str_encode);
 
         dd($ncode);
         $result = $ncode;
