edf198af819c6296b64d7f175d05a581239dfe05
programmerPatient <494429797@qq.com>
2019-11-14 10:27:35 +0800

    s

--- app/Http/Controllers/Adminfour/ReportController.php
+++ app/Http/Controllers/Adminfour/ReportController.php
@@ -495,7 +495,7 @@ class ReportController extends Controller
 
         }
         // dd($response);
-        $encode = mb_detect_encoding($response, array("ASCII",'UTF-8',"GB2312","GBK",'BIG5','blob'));
+        $encode = mb_detect_encoding($response, array("ASCII",'UTF-8',"GB2312","GBK",'BIG5'));
         dd($encode);
         $str_encode = mb_convert_encoding($response, 'UTF-8', $encode);
         // dd($str_encode);
