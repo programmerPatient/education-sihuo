ff5cf76ebc37078c624e2e7e3d6d183e4c58373e
programmerPatient <494429797@qq.com>
2019-11-14 10:47:16 +0800

    s

--- app/Http/Controllers/Adminfour/ReportController.php
+++ app/Http/Controllers/Adminfour/ReportController.php
@@ -501,10 +501,10 @@ class ReportController extends Controller
         // $ss = mb_detect_encoding($str_encode, array("ASCII",'UTF-8',"GB2312","GBK",'BIG5'));
         // dd($ss);
         // dd($str_encode);
-        $ncode = json_decode($response);
+        // $ncode = json_decode($response);
         // dd($str_encode);
-        $image = $ncode;
+        $result['image'] = $str_encode;
         // dd($result);
-        return $image;
+        return $result;
    }
 }
